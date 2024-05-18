644f442e36748b007bbb28bb955fe2aed7c43570
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-05-18 08:44:14 +0300

    feat(lab1React):Slider Class Compoent Function Compoent

---
+++ first_project/src/components/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import './ClassComponent.css'
+
+export default function ClassComponent() {
+    return (
+        <div className="class-component"> 
+            <h1>Class Component</h1>
+        </div>
+    )
+}
---
+++ first_project/src/components/FunctionComponent.jsx
@@ -0,0 +1,9 @@
+import './ClassComponent.css';
+
+export default function FunctionComponent() {
+    return (
+        <div className="function-component">
+            <h1>Function Component</h1>
+        </div>
+    )
+}
